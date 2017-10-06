df697cfff3293e391722a41fa32cf712c6973f93
Heather <hlmathies@gmail.com>
2017-10-06 13:43:51 -0400

    added all keys

--- keys.js
+++ keys.js
@@ -1,20 +1,21 @@
 
+console.log('this is loaded');
 
 var twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: 'mX0o9QE56eMKmppl4uAFfrJfn',
+  consumer_secret: 'ac11WoxvkcpZThNod64qKziqW9pgrB1mhdqUrLgyuiVlq63WQa',
+  access_token_key: '915598833832382465-JDIfTO2fkbYIhtYHfrHmzQ9vTzrd2Fi',
+  access_token_secret: 'XvR8tKNkyKQYj6HDLklSLxGDODcvftMOHCiKAIaWcjavx',
 }
 
 
 var spotifyKeys = {
-    client_id: '',
-    client_secret: ''
+    client_id: '3253473fa6634601855dc5b54fa0f24a',
+    client_secret: '0638d9139a534b91ac5581392f75a222'
 }
 
 var omdbKey = {
-    apiKey: ''
+    apiKey: '40e9cece'
 }
 
 
