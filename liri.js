cd293557d9f19e02861bff7f4f93e05fd089b7f9
Heather <hlmathies@gmail.com>
2017-10-07 14:59:53 -0400

    added basic template for switch statement and function calls

--- liri.js
+++ liri.js
@@ -8,7 +8,27 @@ var keys = require('./keys.js');
 var action = process.argv[2];
 var value = process.argv[3];
 
+/*add switch statements and blank functions, then go back to
+create the functions
+use omdbRequest.js lesson Saturday 21 for OMDB
+use thursday bank.js for a lot of help*/
+switch (action) {
+  case "twitter":
+    twitter();
+    break;
 
+  case "spotify":
+    spotify();
+    break;
+
+  case "omdb":
+    ombd();
+    break;
+
+  case "lotto":
+    lotto();
+    break;
+}
 
 /* 8. Make it so liri.js can take in one of the following commands:
 
@@ -27,7 +47,11 @@ var value = process.argv[3];
 1. `node liri.js my-tweets`
 
    * This will show your last 20 tweets and when they were created at in your terminal/bash window.
+*/
+function twitter() {
 
+};
+/*
 2. `node liri.js spotify-this-song '<song name here>'`
 
    * This will show the following information about the song in your terminal/bash window
@@ -41,7 +65,38 @@ var value = process.argv[3];
      * The album that the song is from
 
    * If no song is provided then your program will default to "The Sign" by Ace of Base.
-   
+   */
+
+   //below is the basic working function that we used in our previous project
+  
+
+  function spotify() { 
+
+    var searchURL = "https://api.spotify.com/v1/search?q=" + value + "&type=playlist&limit=6";
+
+    // console.log(searchURL);
+
+    var spotSearch = {
+    "async": true,
+    "crossDomain": true,
+    "url": searchURL,
+    "method": "GET",
+    "headers": {
+      "authorization": authToken,
+      }
+    }
+    $.ajax(spotSearch).done(function (response) {
+    
+      // console.log(response);
+
+      var musicSearchResults = response.playlists.items;
+
+     
+      };
+ 
+  }; 
+
+  /*
 
 3. `node liri.js movie-this '<movie name here>'`
 
@@ -62,7 +117,23 @@ var value = process.argv[3];
      
      * If you haven't watched "Mr. Nobody," then you should: <http://www.imdb.com/title/tt0485947/>
      
-     * It's on Netflix!
+     * It's on Netflix!*/
+
+     function omdb(){ 
+     // We then run the request module on a URL with a JSON
+request("http://www.omdbapi.com/?t=" + value + "&y=&plot=short&apikey=40e9cece", function(error, response, body) {
+
+  // If there were no errors and the response code was 200 (i.e. the request was successful)...
+  if (!error && response.statusCode === 200) {
+
+    // Then we print out the imdbRating
+    console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
+    /*check the data and then drill into it to display all of the movie details 
+    display all of the movie data using console logs*/
+  }
+ });
+};
+/*
    
 
 4. `node liri.js do-what-it-says`
@@ -74,3 +145,8 @@ var value = process.argv[3];
      * Feel free to change the text in that document to test out the feature for other commands.
 
    */
+
+   function do(){
+
+
+   };
